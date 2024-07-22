c0b7f2c5b59babbc43fd18c75c21bfae5d9fa3ad
Zeriz64 <dark_ghoul16@yahoo.com>
2024-07-22 00:16:44 -0400

    Added Cell class, can now create cells

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from ui import *
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(0, 0), Point(800, 600)), "black")
+    test_cell = Cell(win)
+    test_cell.top_wall = False
+    test_cell.right_wall = False
+    test_cell2 = Cell(win)
+    test_cell2.left_wall = False
+    test_cell2.right_wall = False
+    test_cell3 = Cell(win)
+    test_cell3.bot_wall = False
+    test_cell3.left_wall = False
+    test_cell.draw(2,2, 42,42)
+    test_cell2.draw(42,2, 82,42)
+    test_cell3.draw(82,2, 122,42)
     win.wait_for_close()
 
 main()
--- ui.py
+++ ui.py
@@ -1,5 +1,28 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Cell:
+    def __init__(self, win):
+        self.x1, self.y1 = 0, 0
+        self.x2, self.y2 = 0, 0
+        self.left_wall = True
+        self.right_wall = True
+        self.top_wall = True
+        self.bot_wall = True
+        self.win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self.x1, self.y1 = x1, y1
+        self.x2, self.y2 = x2, y2
+        if self.left_wall:
+            self.win.draw_line(Line(Point(x1, y1), Point(x1, y2)))
+        if self.right_wall:
+            self.win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
+        if self.top_wall:
+            self.win.draw_line(Line(Point(x1, y1), Point(x2, y1)))
+        if self.bot_wall:
+            self.win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -35,5 +58,5 @@ class Window:
     def close(self):
         self.running = False
 
-    def draw_line(self, line, color):
+    def draw_line(self, line, color="black"):
         line.draw(self.canvas, color)
